6f35f646f4185b3c65459c47bb5747f0d390b170
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-20 13:44:39 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -48,6 +48,10 @@ class SongsController < ApplicationController
   end
 
   def upload
+    CSV.foreach(params[:leads].path, headers: true) do |lead|
+      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+    end
+    redirect_to songs_path
   end
 
   private
